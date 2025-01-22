6f8bb26bb567eaec12694ce82f4bbdaa9cc22e8b
abdykadyr <abdukadyrabdilazov@icloud.com>
2025-01-22 17:22:46 +0100

    Added some fragments and almost finish

--- app/src/main/java/com/example/note/auth/AuthFragment.kt
+++ app/src/main/java/com/example/note/auth/AuthFragment.kt
@@ -5,15 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.note.R
+import com.example.note.databinding.FragmentAuthBinding
+import com.example.note.databinding.FragmentThirdBinding
 
 class AuthFragment : Fragment() {
+
+    private var _binding: FragmentAuthBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auth, container, false)
+        _binding = FragmentAuthBinding.inflate(inflater, container, false)
+
+        binding.btnAuth.setOnClickListener {
+            findNavController().navigate(R.id.action_authFragment_to_homeFragment)
+        }
+        return binding.root
     }
 }
--- app/src/main/java/com/example/note/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/note/fragments/HomeFragment.kt
@@ -5,15 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.note.R
+import com.example.note.databinding.FragmentAuthBinding
+import com.example.note.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.btnFab.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_taskFragment)
+        }
+
+        return binding.root
+
+
+
     }
 }
